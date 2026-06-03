065c88ef2fa29ec30efce5171390b5ac8da8d56b
rarfel <faelmvitor@gmail.com>
2026-06-02 23:06:18 -0300

    a simple window implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use glium::Surface;
+
 fn main(){
-    println!("Hello, World!");
+    let event_loop = glium::winit::event_loop::EventLoop::builder()
+        .build()
+        .expect("event loop building");
+
+    let (_windwo, display) = glium::backend::glutin::SimpleWindowBuilder::new()
+        .with_title("Simple Window")
+        .build(&event_loop);
+
+    let mut frame = display.draw();
+    frame.clear_color(0.0, 0.4, 1.0, 1.0);
+    frame.finish().unwrap();
+
+    #[allow(deprecated)]
+    let _ = event_loop.run(move |event, window_target| {
+        match event {
+            glium::winit::event::Event::WindowEvent { event, ..} => match event {
+            glium::winit::event::WindowEvent::CloseRequested => window_target.exit(),
+            _ => (),
+            },
+            _ => (),
+        };
+    })
+    .unwrap();
 }
