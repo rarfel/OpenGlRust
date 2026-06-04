7fed036a024abe030b711c2b7d95c0cb28845b77
rarfel <faelmvitor@gmail.com>
2026-06-03 22:47:33 -0300

    Making a triangle show on screen

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # OpenGl in Rust
 
 ## Objectives
-Find out if trying to compile, code and verify results are easier in rust as oppose of cpp
+Objectives I set to do in this project
+- install rust  
+Worked in the first try
+- prepare OpenGl wrapper
+- create a window
+- make a triangle  
+Simply followed the <a href="https://github.com/glium/glium/blob/master/book/tuto-01-getting-started.md">glium</a> tutorial up to here, I'll still follow it for some time up until I fell confortable with the language
+- implement a general draw function
+- rotate triangle
+- make cube and rotate it
+- create a .obj file reader
+- make .obj appear in screen
+- textures
+- shader the objects
+- make camera movements
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate glium;
 use glium::Surface;
 
 fn main(){
@@ -9,10 +11,55 @@ fn main(){
         .with_title("Simple Window")
         .build(&event_loop);
 
+    // Defining a struct to hold a vertex
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
+    implement_vertex!(Vertex, position);
+
+    // making a tringle manually
+    let shape = vec![
+        Vertex{position:[-0.5, -0.5]},
+        Vertex{position:[0.0, 0.5]},
+        Vertex{position:[0.5, -0.25]}
+    ];
+
+    // creating a buffer to store the triengle
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    // shader to render the triangle
+
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+
+        out vec4 color;
+
+        void main(){
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+    // draw function
     let mut frame = display.draw();
     frame.clear_color(0.0, 0.4, 1.0, 1.0);
+    frame.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
     frame.finish().unwrap();
 
+    // event loop
     #[allow(deprecated)]
     let _ = event_loop.run(move |event, window_target| {
         match event {
