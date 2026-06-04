d670662ef4c57ed12a03b5849c55566eaa0549c7
rarfel <faelmvitor@gmail.com>
2026-06-04 14:52:08 -0300

    Animating the triangle rotate and fixing spelling mistakes

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Worked in the first try
 - prepare OpenGl wrapper
 - create a window
 - make a triangle  
-Simply followed the <a href="https://github.com/glium/glium/blob/master/book/tuto-01-getting-started.md">glium</a> tutorial up to here, I'll still follow it for some time up until I fell confortable with the language
+Simply followed the <a href="https://github.com/glium/glium/blob/master/book/tuto-01-getting-started.md">glium</a> tutorial up to here, I'll still follow it for some time up until I feel confortable with the language
 - implement a general draw function
 - rotate triangle
 - make cube and rotate it
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main(){
         .build()
         .expect("event loop building");
 
-    let (_windwo, display) = glium::backend::glutin::SimpleWindowBuilder::new()
+    let (_window, display) = glium::backend::glutin::SimpleWindowBuilder::new()
         .with_title("Simple Window")
         .build(&event_loop);
 
@@ -17,17 +17,6 @@ fn main(){
         position: [f32; 2],
     }
     implement_vertex!(Vertex, position);
-
-    // making a tringle manually
-    let shape = vec![
-        Vertex{position:[-0.5, -0.5]},
-        Vertex{position:[0.0, 0.5]},
-        Vertex{position:[0.5, -0.25]}
-    ];
-
-    // creating a buffer to store the triengle
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
     // shader to render the triangle
 
@@ -53,19 +42,43 @@ fn main(){
 
     let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
-    // draw function
-    let mut frame = display.draw();
-    frame.clear_color(0.0, 0.4, 1.0, 1.0);
-    frame.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
-    frame.finish().unwrap();
-
+    // variable to control the animation of the scene
+    let mut t:f32 = 0.0;
     // event loop
     #[allow(deprecated)]
     let _ = event_loop.run(move |event, window_target| {
         match event {
             glium::winit::event::Event::WindowEvent { event, ..} => match event {
-            glium::winit::event::WindowEvent::CloseRequested => window_target.exit(),
+                glium::winit::event::WindowEvent::CloseRequested => window_target.exit(),
+                glium::winit::event::WindowEvent::Resized(window_size)=>{
+                    display.resize(window_size.into());
+                }
+                glium::winit::event::WindowEvent::RedrawRequested => {
+                    t += 0.02;
+                    let x_off = t.cos() * 0.5;
+                    let y_off = t.sin() * 0.5;
+
+                    // making a triangle manually
+                    let shape = vec![
+                        Vertex{position:[-0.5 + x_off, -0.5 + y_off]},
+                        Vertex{position:[0.0 + x_off, 0.5 + y_off]},
+                        Vertex{position:[0.5 + x_off, -0.25 + y_off]}
+                    ];
+
+                    // creating a buffer to store the triangle
+                    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+                    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+                    // draw function
+                    let mut frame = display.draw();
+                    frame.clear_color(0.0, 0.4, 1.0, 1.0);
+                    frame.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
+                    frame.finish().unwrap();
+                },
             _ => (),
+            },
+            glium::winit::event::Event::AboutToWait => {
+                _window.request_redraw();
             },
             _ => (),
         };
