4ffb1d60018afcf08c16632c731870d1a7c7d046
rarfel <faelmvitor@gmail.com>
2026-06-08 20:18:06 -0300

    implemented backface culling

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -11,5 +11,5 @@ uniform mat4 matrix;
 void main() {
   float scaler = 0.01;
   v_normal = transpose(inverse(mat3(matrix))) * normal;
-  gl_Position = projection * matrix * vec4(position * scaler, 1.0);
+  gl_Position = matrix * projection * vec4(position * scaler, 1.0);
 }
